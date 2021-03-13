a2a443f23fcef6790dc9910142a0fd8165b29b69
Joe Basquez <jybasquez@yahoo.com>
2021-03-13 11:03:29 -0500

    connecting to local host and creating db

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
-    username: process.env.SEQUELIZE_USER,
-    password: process.env.SEQUELIZE_PASSWORD,
+    username: process.env.SEQUELIZE_USER || 'root',
+    password: process.env.SEQUELIZE_PASSWORD || '12345678',
     database: 'project2_dev',
     dialect: 'mysql',
     host: process.env.SEQUELIZE_HOST,
